ce56849ec7de0eb08cbadb5a1369b1d68b78b93f
gtopinio <mctopinio@up.edu.ph>
2024-06-15 23:36:03 +0800

    update: compartmentalize registration page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors : {
+        'creamy-gray': '#D9D9D9',
+      },
       animation: {
         'fade-in': 'fadeIn 0.5s ease-in forwards',
         'zoom-fade-in': 'zoomFadeIn 1s ease-out forwards',
